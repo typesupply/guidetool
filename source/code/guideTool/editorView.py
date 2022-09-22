307fca4db638b470466b2c261b8b488f6f5e694a
Tal Leming <tal@typesupply.com>
2022-09-22 13:06:10 -0400

    Make sure number is the proper type.
    This should close #21.

--- source/code/guideTool/editorView.py
+++ source/code/guideTool/editorView.py
@@ -10,6 +10,15 @@ from .compatibility import getGuidelineLibValue, setGuidelineLibValue
 numberTextFieldWidth = 50
 noColor = (1.0, 1.0, 1.0, 1.0)
 
+def convertNumberType(number):
+    if not isinstance(number, (int, float)):
+        number = float(number)
+    if not isinstance(number, int):
+        asInt = int(number)
+        if asInt == number:
+            number = asInt
+    return number
+
 class GuideToolGuidelineEditor(ezui.TwoColumnForm):
 
     font = None
@@ -172,12 +181,15 @@ class GuideToolGuidelineEditor(ezui.TwoColumnForm):
         x = data["xPositionTextField"]
         if x is None:
             x = guideline.x
+        x = convertNumberType(x)
         y = data["yPositionTextField"]
         if y is None:
             y = guideline.y
+        y = convertNumberType(y)
         angle = data["angleTextField"]
         if angle is None:
             angle = guideline.angle
+        angle = convertNumberType(angle)
         color = data["colorColorWell"]
         if color == noColor:
             color = None
